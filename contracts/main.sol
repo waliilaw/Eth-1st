0593039c04a843d330dfb6546e71e1d413457aa5
Wali <humaidwali20@gmail.com>
2025-09-15 12:29:05 +0530

    new

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,47 +1,61 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-MIT
 pragma solidity ^0.8.0;
 
 contract Lottery {
+
+    event PlayerEntered(address player , uint amount);
+    event WinnerPicked(address winner , uint prize);
+
+
     address public manager;
     address[] public players;
     uint public entryFee = 0.1 ether;
-    
-    constructor() {
-        manager = msg.sender;
-    }
-    
-    function enter() public payable {
-        require(msg.value == entryFee, "Entry fee is 0.1 ether");
-        players.push(msg.sender);
-    }
-    
-    function getPlayers() public view returns (address[] memory) {
-        return players;
-    }
-    
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
-    
-    function pickWinner() public restricted {
-        require(players.length > 0, "No players in lottery");
-        
-        uint index = random() % players.length;
-        address payable winner = payable(players[index]);
-        
 
-        winner.transfer(address(this).balance);
-        
+constructor() {
+    manager = msg.sender;
+}
 
-        players = new address[](0);
-    }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
-    }
-    
-    modifier restricted() {
-        require(msg.sender == manager, "Only manager can call this");
-        _;
-    }
+function enter() public payable{
+    require(msg.value == entryFee , "Entry Fee is 0.1 ETH");
+    players.push(msg.sender);
+
+    emit PlayerEntered(msg.sender , msg.value);
+}
+
+function viewPlayers() public view returns(address[] memory){ 
+    return players;
+}
+
+function viewBalance() public view returns(uint){
+    return address(this).balance;
+}
+
+function randomSelection() public view returns(uint){
+    return uint(
+        keccak256(abi.encodePacked(
+            block.difficulty,
+            block.timestamp,
+            players
+        )));
+}
+
+modifier onlyManager() {
+    require(msg.sender == manager , "Only Founder can call this , hehe :]");
+    _;
+}
+
+function pickWinner() public onlyManager{
+    require(players.length > 0 , "No Player Found in the lobby , sed life :(");
+
+    uint index = randomSelection() % players.length;
+    address payable winner = payable(players[index]);
+    uint prize = address(this).balance;
+
+    winner.transfer(prize);
+    emit WinnerPicked(winner , prize);
+
+    players = new address[](0);
+}
+
+
 }
